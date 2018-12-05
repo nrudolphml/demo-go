2ea69eda53e354d968cce45e5ed17db783ea1c8d
Nicolas Rudolph <nicolas.rudolph@mercadolibre.com>
2018-12-05 11:35:09 -0300

    first commit

---
+++ src/service/tweet_manager_test.go
@@ -0,0 +1,14 @@
+package service_test
+
+import(
+  "testing"
+  "github.com/nrudolph/twitter/src/service"
+)
+
+func TestPublishedTweetIsSaved(t *testing.T){
+  var tweet string = "this is my first tweet"
+  service.PublishTweet(tweet)
+  if service.Tweet != tweet {
+    t.Error("Expected tweet is", tweet)
+  }
+}
